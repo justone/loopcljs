6a692cd23147987ec626204d35ac1dc49aa32a6f
Nate Jones <nate@endot.org>
2015-01-30 11:11:00 -0800

    switch from let to thread macro

--- src/cljs/loopcljs/core.cljs
+++ src/cljs/loopcljs/core.cljs
@@ -2,7 +2,8 @@
   (:require [om.core :as om :include-macros true]
             [om-tools.dom :as dom :include-macros true]
             [clojure.string :as string]
-            [goog.net.Jsonp :as jsonp]))
+            [goog.net.Jsonp :as jsonp]
+            [clojure.walk :as walk]))
 
 (def app-state (atom {:images ()}))
 
@@ -12,14 +13,15 @@
     (.send jsonp
            (clj->js {"sort" "top" "t" "week"})
            (fn [datajs]
-             (let [data      (js->clj datajs)
-                   entries   (get-in data ["data" "children"])
-                   pg13      (remove #(get-in % ["data" "over_18"]) entries)
-                   images    (map #(get-in % ["data" "url"]) pg13)
-                   just-gifs (filter #(re-matches #".*\.gifv?$" %) images)
-                   no-gifvs  (map #(string/replace % "gifv" "gif") just-gifs)]
-               ;(js/console.log (str no-gifvs))
-               (swap! app-state assoc :images no-gifvs))))))
+             (let [data    (walk/keywordize-keys (js->clj datajs))
+                   entries (get-in data [:data :children])]
+               (->> entries
+                    (map :data)
+                    (remove :over_18)
+                    (map :url)
+                    (filter #(re-matches #".*\.gifv?$" %))
+                    (map #(string/replace % "gifv" "gif"))
+                    (swap! app-state assoc :images)))))))
 
 (defn main []
 
